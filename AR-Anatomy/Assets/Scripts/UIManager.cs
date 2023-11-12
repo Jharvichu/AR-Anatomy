96fd36cd4aa18a289ad7dde61750f0c10e14ba5c
Jharvy Cadillo <jharvichu@hotmail.com>
2023-11-12 13:16:23 -0500

    Interfaz funcional
    Hice el interfaz para poner los modelos.
    -Inicio con un boton
    -menu principal con el boton de modelos, extra y de configuracion.
    (configuracion tiene boton de salir, inicio y extra)
    -PonerPosicionAR: solamente estara la opcion de poner el modelo 3d en el espacio

---
+++ AR-Anatomy/Assets/Scripts/UIManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject inicioCanvas;
+    [SerializeField]
+    private GameObject menuPrincipalCanvas;
+    [SerializeField]
+    private GameObject posicionModelosCanvas;
+
+
+
+    void Start()
+    {
+        GameManager.instance.OnStart += ActivateStartApp;
+        GameManager.instance.OnMainMenu += ActivateMainMenu;
+        GameManager.instance.OnARPosition += ActivateARPosition;
+    }
+
+    private void ActivateARPosition()
+    {
+        inicioCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
+        inicioCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
+
+        menuPrincipalCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(3).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(4).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+
+        posicionModelosCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+        posicionModelosCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+    }
+
+    private void ActivateStartApp()
+    {
+
+        inicioCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
+        inicioCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
+
+        menuPrincipalCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(3).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(4).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+
+        posicionModelosCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        posicionModelosCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+
+
+    }
+
+    private void ActivateMainMenu()
+    {
+        inicioCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
+        inicioCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
+
+        menuPrincipalCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(2).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(3).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+        menuPrincipalCanvas.transform.GetChild(4).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+
+        posicionModelosCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+        posicionModelosCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
+    }
+
+   
+}
