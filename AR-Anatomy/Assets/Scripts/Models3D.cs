a4e54f9bc16c881070db3321f7742e7c07f761d9
Jharvy Cadillo <jharvichu@hotmail.com>
2023-11-21 14:47:14 -0500

    scrpts gaaaaaaaa

---
+++ AR-Anatomy/Assets/Scripts/Models3D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Models3D : ScriptableObject
+{
+    public string ModelName;
+    public Sprite ModelImage;
+    public GameObject Model3D;
+}
---
+++ AR-Anatomy/Assets/Scripts/ModelsButton.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModelsButton : MonoBehaviour
+{
+    private string modelName;
+    private Sprite modelImage;
+    private GameObject model3D;
+    public string ModelName { set => modelName = value; }
+    public Sprite ModelImage { set => modelImage = value; }
+    public GameObject Model3D { set => model3D = value; }
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.GetChild(0).GetComponent<Text>().text = modelName;
+        transform.GetChild(0).GetComponent<RawImage>().texture = modelImage.texture;
+
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(GameManager.instance.ARPosition);
+        button.onClick.AddListener(Create3DModel);
+
+    }
+
+    private void Create3DModel()
+    {
+        Instantiate(model3D);
+    }
+
+}
